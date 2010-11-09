14f99de324fb36ff49aa73c95555f5862b07c659
Ezequiel <exe.zaid@gmail.com>
2010-11-09 00:01:58 -0300

    index = project index

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Proyecto::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "projects#index"
 
   # See how all your routes lay out with "rake routes"
 
@@ -61,3 +61,4 @@ Proyecto::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
