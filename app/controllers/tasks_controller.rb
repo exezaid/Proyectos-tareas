6231599601342d2961ea0101da576607b91c698d
Ezequiel <exe.zaid@gmail.com>
2010-11-08 22:09:15 -0300

    fix destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -83,7 +83,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     respond_to do |format|
-      format.html { redirect_to(tasks_url) }
+      format.html { redirect_to([@project, @task]) }
       format.xml  { head :ok }
     end
   end
